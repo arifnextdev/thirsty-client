e99ccdaea36a9ca6e28a26a28f15046bbab469c6
ariful171042 <arif171042@gmail.com>
2023-11-06 00:35:25 +0600

    hero done

--- app/(home)/_components/Slider.tsx
+++ app/(home)/_components/Slider.tsx
@@ -5,8 +5,16 @@ import { Swiper,SwiperSlide } from "swiper/react"
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-
 import { Navigation,Pagination,Autoplay } from 'swiper/modules';
+import { data } from "@/data/sliderContents";
+import Image from "next/image";
+
+const sliderContents = [{
+  image:'',
+  heading:'',
+  subHeading:''
+}]
+
 
 const Slider = () => {
   return (
@@ -15,8 +23,16 @@ const Slider = () => {
             delay:3000,
             disableOnInteraction:false
           }} navigation={true} modules={[Navigation,Pagination,Autoplay]} className="mySwiper w-full h-full">
-        <SwiperSlide>Slide</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
+
+            {
+              data?.map(slide=>(
+              <SwiperSlide key={slide.heading}><Image src={slide.image} width={500} height={500} alt={slide.heading} className="w-full h-full object-cover"/>
+              <h1>{slide.heading}</h1>
+              </SwiperSlide>
+              ))
+            }
+       
+        
       </Swiper>
     </section>
   )
