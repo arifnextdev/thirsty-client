92112221f4ca74ab4b481bf7da1983def99d865a
ariful171042 <arif171042@gmail.com>
2023-12-08 23:03:36 +0600

    user page

--- app/admin/manage/users/_components/User.tsx
+++ app/admin/manage/users/_components/User.tsx
@@ -97,6 +97,21 @@ const User: React.FC<UserProps> = ({ users, currentUser, token }) => {
                     {user.name}
                   </div>
                 )}
+                {isModalOpen && (
+                  <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
+                    {user.name}
+                  </div>
+                )}
+                {isModalOpen && (
+                  <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
+                    {user.name}
+                  </div>
+                )}
+                {isModalOpen && (
+                  <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
+                    {user.name}
+                  </div>
+                )}
               </>
             ))}
           </tbody>
